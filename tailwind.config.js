60ac36f005fe8f7ed0ebeb603036bfe00d998fbb
Sussmrithi <sussmrithi@gmail.com>
2025-04-10 02:43:38 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'apple-gray': '#1d1d1f',
+        'apple-blue': '#2997ff',
+      },
+      fontFamily: {
+        'bodoni': ['Libre Bodoni', 'serif'],
+        'garamond': ['EB Garamond', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+};
